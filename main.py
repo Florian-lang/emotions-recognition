8f48e07a621e202af3892c040bc2ca6faee68956
Florian Baumard <florianbaumard@macbook-pro-de-florian.home>
2023-11-26 17:54:24 +0100

    fix volume issue for docker container

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import numpy as np
-import tensorflow as tf
-import matplotlib.pyplot as plt
-from keras.models import Sequential
-from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
-
 # Create the datasets
 if __name__=="__main__":
     print("hello world ->", __name__)
