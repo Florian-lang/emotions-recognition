721a88dbe02e4caa881ae695124b3a5c236e2a75
Nunez Dorian <doriannunez64@gmail.com>
2024-01-12 20:14:00 +0100

    Change required

---
+++ application/camera.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+from keras.models import load_model
+from keras.applications.vgg16 import preprocess_input
+
+model = load_model('emotion_recognition_model.h5')
+
+emotionLabels = ['Anger', 'Contempt', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
+
+def openCamera():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        faces = face_cascade.detectMultiScale(frame, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            capturedFace = frame[y:y+h, x:x+w]
+
+            resizedFace = cv2.resize(capturedFace, (96, 96))
+            reshapeFace = preprocess_input(resizedFace).reshape(96, 96, 3)
+            expendedFaceDimenssion = np.expand_dims(reshapeFace, axis=0)
+
+            emotion = model.predict(expendedFaceDimenssion)
+
+            emotionName = emotionLabels[np.argmax(emotion)]
+
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+            cv2.putText(frame, emotionName, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+        cv2.imshow('Emotion Recognition', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ train/train.py
@@ -0,0 +1,52 @@
+from keras.applications import MobileNet
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.models import Model
+from keras.preprocessing.image import ImageDataGenerator
+
+# Création de paginateur pour normaliser les pixels entre 0 et 1
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# Création des données d'entrainement
+train_generator = train_datagen.flow_from_directory(
+        'dataset/train',
+        target_size=(96, 96),
+        batch_size=32,
+        class_mode='categorical')
+
+# Création des données de test
+validation_generator = test_datagen.flow_from_directory(
+        'dataset/test',
+        target_size=(96, 96),
+        batch_size=32,
+        class_mode='categorical')
+
+# Création du modèle
+base_model = MobileNet(weights='imagenet', include_top=False, input_shape=(96, 96, 3))
+
+# On ajoute les couches de classification
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+
+x = Dense(1024, activation='relu')(x)
+
+predictions = Dense(train_generator.num_classes, activation='softmax')(x)
+
+# On définit le modèle custom
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# On compile le modèle notre optimiseur, notre fonction de perte et nos métriques
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Entrainement du modèle
+model.fit(
+        train_generator,
+        steps_per_epoch=train_generator.samples // train_generator.batch_size,
+        epochs=10,
+        validation_data=validation_generator,
+        validation_steps=validation_generator.samples // validation_generator.batch_size,
+        verbose=2
+)
+
+# Sauvegarde du modèle
+model.save('emotion_recognition_model.h5')
