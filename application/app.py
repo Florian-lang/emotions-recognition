feeae5a1a848112282b1bf96141339dd009c8205
Florian Baumard <florianbaumard4@gmail.com>
2024-01-29 10:00:06 +0100

    fix image uploading

--- application/app.py
+++ application/app.py
@@ -7,7 +7,15 @@ import camera as camera
 
 
 def open_image():
-    selected_file_path = filedialog.askopenfilename(title="Sélectionner une image", filetypes=[("Image files", "*.png;*.jpg;*.jpeg;")])
+    selected_file_path = filedialog.askopenfilename(
+        title="Sélectionner une image",
+        filetypes=[
+            ("Image files", "*.png"),
+            ("Image files", "*.jpg"),
+            ("Image files", "*.jpeg")
+        ]
+    )
+
     if selected_file_path:
         emotion, emotion_name = Rd.emotion_on_image(selected_file_path)
         show_image(emotion)
@@ -25,23 +33,20 @@ def show_image(img):
     img.thumbnail((900, 900))
     img = ImageTk.PhotoImage(img)
 
-    # Afficher l'image dans une fenêtre Tkinter
     imgLabel.configure(image=img)
     imgLabel.image = img
 
 
 def show_video(file_path):
-    cap = cv2.VideoCapture(file_path)  # Define the variable "cap" to capture video from the default camera
+    cap = cv2.VideoCapture(file_path)
 
     _, frame = cap.read()
 
-    # Convertir l'image de BGR à RGB pour l'affichage dans Tkinter
     emotion, emotion_name = Rd.emotion_on_image(file_path, frame)
     show_image(emotion)
     emotionNameLabel.configure(text=emotion_name)
     emotionNameLabel.text = emotion_name
 
-    # Fonction pour lire la vidéo
     def update_video():
         nonlocal frame
         ret, frame = cap.read()
@@ -51,9 +56,8 @@ def show_video(file_path):
             emotionNameLabel.configure(text=emotion_name)
             emotionNameLabel.text = emotion_name
             emotion.thumbnail((900, 900))
-            imgLabel.after(10, update_video)  # Mise à jour toutes les 10 ms
+            imgLabel.after(10, update_video)
 
-    # Démarrer la lecture de la vidéo
     update_video()
 
 
@@ -61,28 +65,23 @@ def open_camera():
     camera.open_camera()
 
 
-# Créer une fenêtre Tkinter
 fenetre = tk.Tk()
 fenetre.geometry("300x300")
 fenetre.title("Sélection d'image et de vidéo")
 
-# Bouton pour insérer une image
 btnImage = tk.Button(fenetre, text="Insérer une image", command=open_image)
 btnImage.pack(pady=10)
 
-# Bouton pour ouvrir une vidéo
 btnVideo = tk.Button(fenetre, text="Ouvrir une vidéo", command=open_video)
 btnVideo.pack(pady=10)
 
 btnCamera = tk.Button(fenetre, text="Ouvrir la camera", command=open_camera)
 btnCamera.pack(pady=10)
 
-# Label pour afficher l'image ou la vidéo
 imgLabel = tk.Label(fenetre)
 imgLabel.pack()
 
 emotionNameLabel = tk.Label(fenetre)
 emotionNameLabel.pack()
 
-# Lancer la boucle principale Tkinter
 fenetre.mainloop()
